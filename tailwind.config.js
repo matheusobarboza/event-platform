82a6873fd7abb2ff56537188938e6c5be5c6ec34
Matheus Barboza <matheus.barboza102@gmail.com>
2022-06-22 19:32:13 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx' //todos os arquivos com extensão .tsx vão ter estilização do tailwindcss
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
